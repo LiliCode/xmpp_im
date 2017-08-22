ac2c4d3967fa84b50a534e49402ac5d8a6f3676e
ldl <542155086@qq.com>
2017-08-22 11:39:17 +0800

    Initial Commit

---
+++ xmpp_im/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmpp_im
+//
+//  Created by winter on 2017/8/22.
+//  Copyright © 2017年 laidongling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xmpp_im/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xmpp_im
+//
+//  Created by winter on 2017/8/22.
+//  Copyright © 2017年 laidongling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
